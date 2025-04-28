1c411b207b28f63973140eb85258ecc0bcbeebb8
Kingfisher9484 <kingfisher9484@gmail.com>
2025-04-28 15:02:49 +0530

    Allowed Render

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from "path";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   // server:{
+//   //   host:"0.0.0.0",
+//   //   fs:{
+//   //     strict:false
+//   //   },
+//   // },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // server:{
-  //   host:"0.0.0.0",
-  //   fs:{
-  //     strict:false
-  //   },
-  // },
+  server: {
+    allowedHosts: ['celehub.onrender.com'], // Add the blocked host here
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   plugins: [react()],
-})
+});
